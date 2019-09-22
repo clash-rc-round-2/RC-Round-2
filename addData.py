a52ff5f5a1ccf423c5bf1cd2c6674dfa34697d06
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-09-22 08:06:21 +0530

    change addData

--- addData.py
+++ addData.py
@@ -10,14 +10,15 @@ QUESTIONS = 8
 
 path = os.environ['HOME'] + '/Description/'
 
+os.system("mkdir data/usersCode")
 os.system("python3 manage.py makemigrations userApp")
 os.system("python3 manage.py migrate")
 
 
-for filename in os.listdir(path):
-    file = open(path + filename, 'r')
-    content = file.read()
-    print(str(content))
-    question = Question(question=content, titleQue=filename)
-    question.save()
-    file.close()
+# for filename in os.listdir(path):
+#     file = open(path + filename, 'r')
+#     content = file.read()
+#     print(str(content))
+#     question = Question(question=content, titleQue=filename)
+#     question.save()
+#     file.close()
